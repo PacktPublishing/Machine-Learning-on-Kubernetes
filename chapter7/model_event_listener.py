7f78b24fab5330a3739353e610ea2862d50baa6b
Faisal Masood <fmasood@redhat.com>
2022-01-10 16:18:21 +1100

    added doco 👽

--- chapter7/model_event_listener.py
+++ chapter7/model_event_listener.py
@@ -7,8 +7,11 @@ url = os.getenv("TARGET_SERVER_URL") or "http://127.0.0.1:8080"
 loop = asyncio.get_event_loop()
 
 
-async def outlier_call(payload, request_headers):
-    print("asdadasd")
+async def call_outlier_model(payload, request_headers):
+    """ This method makes a call to another model, in our case the outlier detector, and prints the response
+        In a production scenario, instead of printing the response, if an outlier has been detected this method
+        can trigger an event which can then be handled as needed
+    """
     async with httpx.AsyncClient() as client:
         print("Calling")
         response = await client.post(url, json=payload)
@@ -16,7 +19,10 @@ async def outlier_call(payload, request_headers):
 
 
 class SeldonCoreHTTPRequestHandler(BaseHTTPRequestHandler):
-
+    """ A simple HTTP server which will receive the input payload from Seldon core. The server will respond immediately as HTTP.200
+        It will then call the outlier model to record if there is an outlier payload is detected. Right now the async calls are managed in memory,
+        however, in a production scenario, an event bus would be a better choice.
+    """
     def do_POST(self):
         try:
             request_headers = self.headers
@@ -26,7 +32,7 @@ class SeldonCoreHTTPRequestHandler(BaseHTTPRequestHandler):
             payload = json.loads(self.rfile.read(length))
             print(f"payload is {payload}")
 
-            future = asyncio.ensure_future(outlier_call(payload, request_headers), loop=loop)
+            future = asyncio.ensure_future(call_outlier_model(payload, request_headers), loop=loop)
             loop.run_until_complete(future)
 
         except Exception as ex:
